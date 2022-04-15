e12eff9c2ec8d1c83f8672d300687571bf52700f
Tithian <31572816+Tithian@users.noreply.github.com>
2022-04-15 23:26:21 +0200

    pavic

--- ejjs/02/script.js
+++ ejjs/02/script.js
@@ -1,100 +1,34 @@
-//
-//var bind = Function.prototype.bind,
-//  $append = bind.call(Element.prototype.appendChild, document.querySelector("output")),
-//  $erase = bind.call(Element.prototype.removeChild, document.querySelector("output")),
-//  $new = bind.call(Document.prototype.createElement, document),
-//  $text = bind.call(Document.prototype.createTextNode, document),
-//  $rnd = function () {return (Math.random() * 125 + 0) | 0;},
-//  $promise = function (thenFn) {
-//    var args,
-//      promise,
-//      wait,
-//      slice = Array.prototype.slice,
-//      isResolved = false;
-//    var promise = {
-//      clear: function () {
-//        rmv = $remove;
-//        return promise;
-//      },
-//      wait: function (ms) {
-//        wait = ms;
-//        return promise;
-//      },
-//      then: function () {
-//        args = slice.call(arguments);
-//        return (promise = $promise(thenFn));
-//      },
-//      resolve: function () {
-//        isResolved = true;
-//        if (args) {
-//          var next = Function.prototype.bind.apply(
-//            thenFn,
-//            [undefined].concat(args).concat([promise])
-//          );
-//          wait ? setTimeout(next, wait) : next();
-//        }
-//      }
-//    };
-//    return promise;
-//  };
-//
-//var process = function (target, chars, promise) {
-//  var first = chars[0],
-//    rest = chars.slice(1);
-//  if (!first) {
-//    promise.resolve();
-//    return;
-//  }
-//  target.appendChild(first);
-//  setTimeout(process.bind(undefined, target, rest, promise), $rnd());
-//};
-//
-//var type = function (text, promise) {
-//  var chars = text.split("").map($text);
-//  promise = promise || $promise(type);
-//  $append($new("br"));
-//  process($append($new("q")), chars, promise);
-//  return promise;
-//};
-//
-//type("Testing....")
-//  .wait(500)
-//  .then("This is just a test...")
-//  .then("This is *only* a test...")
-//  .wait(1500)
-//  .then(
-//    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae tellus aliquet, eleifend mi id, malesuada massa. Nam sit amet nunc ut nulla elementum tempor eu ut ipsum. Suspendisse eget augue sollicitudin, adipiscing dui sed, blandit nulla. Donec tempor vehicula nisi at molestie. Vestibulum interdum sem arcu, vitae sodales libero gravida et. Suspendisse pretium consectetur augue at lacinia. Ut urna lectus, tincidunt ut elit vitae, egestas egestas ipsum. Maecenas gravida tortor eget metus pulvinar sodales. Cras vitae nisi nec felis varius auctor. Donec semper aliquam mollis."
-//  )
-//  .wait(1000)
-//  .clear();
+var $rnd = function() {return (Math.random() * 50 + 25) | 0;};
 
-var $rnd = function() {return (Math.random() * 125 + 0) | 0;};
-
-const TypeWriter = function(txtElement, texto, wait = $rnd()) {
-    this.txtElement = txtElement;
-    this.texto = texto;
+const TypeWriter = function(contenedor, texto, wait = $rnd()) {
+    this.contenedor = contenedor;
     this.wait = wait;
+    this.texto = texto;
+    this.indice = 0;
+    this.finish = texto.length;
     this.type();
+        console.log("Contenedor: "+this.contenedor);
+        console.log("Texto:"+this.texto);
+        console.log(this.finish);
     this.isDeleting = false;
 }
 // Type Method
 TypeWriter.prototype.type = function () {
-    const fullTxt = this.texto
-    // Comprobando si borra o no
-    if(this.isDeleting) {
-        // Borra un caracter
-        this.txt = fullTxt[0];
-    } else {
-        // Añade caracter
-        this.txt = fullTxt[0];
-    }
+    console.log("ACTUAL: "+this.indice);
+    console.log("Total: "+this.finish);
+
     // Inserta texto en el HTML
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+    if(this.indice<this.finish) {
+        this.contenedor.innerHTML += `<span class="txt">${this.texto[this.indice]}</span>`;
+        this.indice++;
+    }
 
     // Velocidad
     let typeSpeed = $rnd();
 
-    if(this.isDeleting) {
+    setTimeout(() => this.type(), this.wait);
+
+/*    if(this.isDeleting) {
         typeSpeed /= 2;
     }
 
@@ -108,7 +42,7 @@ TypeWriter.prototype.type = function () {
         this.isDeleting = false;
         // Pausa antes de escribir
         typeSpeed *= 4;
-    }
+    }*/
 
 }
 
@@ -117,43 +51,133 @@ document.addEventListener('DOMContentLoaded', init);
 
 // Init App
 function init() {
-    const txtElement = document.querySelector("output");
-    const texto = "Probando 1 2 3";
+    const contenedor = document.getElementById("consola");
+    const texto = "Bienvenido al examen de comandos de Linux Rosalía \"Dark Souls Distrb.\". Introduce un comando que pare la guerra en Ucrania, que busque en el mundo solo las bolas de dragón comprendidas entre 2-5 estrellas ordenado de forma inversa y envía el documento sin machacar su contenido al correo de Joe-Biden con el asunto \"Que te jodan Biden\". Ovbiamente todo en una sola línea.";
     wait  = $rnd();
 
-    new TypeWriter(txtElement, texto, wait);
+    new TypeWriter(contenedor, texto, wait);
 }
 
 /*
+// const TypeWriter = function(txtElement, words, wait = 3000) {
+//   this.txtElement = txtElement;
+//   this.words = words;
+//   this.txt = '';
+//   this.wordIndex = 0;
+//   this.wait = parseInt(wait, 10);
+//   this.type();
+//   this.isDeleting = false;
+// }
 
-var salida = document.querySelector("#salida");
-var br = document.createElement("br");
-var linea = document.createElement("q");
-var consola = document.getElementById("consola");
+// // Type Method
+// TypeWriter.prototype.type = function() {
+//   // Current index of word
+//   const current = this.wordIndex % this.words.length;
+//   // Get full text of current word
+//   const fullTxt = this.words[current];
 
-function rand() {
-    return (Math.random() * 125 + 0) | 0;
-}
+//   // Check if deleting
+//   if(this.isDeleting) {
+//     // Remove char
+//     this.txt = fullTxt.substring(0, this.txt.length - 1);
+//   } else {
+//     // Add char
+//     this.txt = fullTxt.substring(0, this.txt.length + 1);
+//   }
 
-function appending(element) {
-    salida.appendChild(element);
-}
+//   // Insert txt into element
+//   this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-function escrib(letra) {
-    setTimeout(consola.innerHTML+=letra, 100);
-}
+//   // Initial Type Speed
+//   let typeSpeed = 300;
+
+//   if(this.isDeleting) {
+//     typeSpeed /= 2;
+//   }
+
+//   // If word is complete
+//   if(!this.isDeleting && this.txt === fullTxt) {
+//     // Make pause at end
+//     typeSpeed = this.wait;
+//     // Set delete to true
+//     this.isDeleting = true;
+//   } else if(this.isDeleting && this.txt === '') {
+//     this.isDeleting = false;
+//     // Move to next word
+//     this.wordIndex++;
+//     // Pause before start typing
+//     typeSpeed = 500;
+//   }
+
+//   setTimeout(() => this.type(), typeSpeed);
+// }
 
 
+// ES6 Class
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
 
-appending(br);
-appending(linea);
-var enun = "Erase una vez";
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
 
-function write() {
-    for (let i = 0; i < enun.length; i++) {
-        linea;
-        linea.id = "q"+i;
-        setTimeout(document.getElementById("q"+i).innerHTML = enun[i], 1000);
+    // Check if deleting
+    if(this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 300;
+
+    if(this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if(!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if(this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
 }
 */
